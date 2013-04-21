1d74e54472f5a513e0f7e854599760b5e112e318
Preben Asmussen <preben.asmussen@gmail.com>
2013-04-21 17:00:25 +0200

    upgrade to camel 2.11.0

--- src/main/java/org/apache/camel/splunk/demo/Tweet2SplunkEvent.java
+++ src/main/java/org/apache/camel/splunk/demo/Tweet2SplunkEvent.java
@@ -3,18 +3,18 @@ package org.apache.camel.splunk.demo;
 import org.apache.camel.Converter;
 import org.apache.camel.component.splunk.event.SplunkEvent;
 
-import twitter4j.Tweet;
+import twitter4j.Status;
 
 @Converter
 public class Tweet2SplunkEvent {
     @Converter
-    public static SplunkEvent convertTweet(Tweet tweet) {
+    public static SplunkEvent convertTweet(Status status) {
         SplunkEvent data = new SplunkEvent("twitter-message", "twitter");
-        data.addPair("source", tweet.getSource());
-        data.addPair("from_user", tweet.getFromUser());
-        data.addPair("from_user_id", tweet.getFromUserId());
-        data.addPair("created_at", tweet.getCreatedAt());
-        data.addPair("text", tweet.getText());
+        data.addPair("source", status.getSource());
+        data.addPair("from_user", status.getUser());
+        data.addPair("from_user_id", status.getId());
+        data.addPair("created_at", status.getCreatedAt());
+        data.addPair("text", status.getText());
         return data;
     }
 }
