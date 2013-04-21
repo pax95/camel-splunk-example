3e70b8707d18821c08eddee64907fb61c8501462
Preben Asmussen <preben.asmussen@gmail.com>
2013-04-21 20:16:51 +0200

    more tweet attributes

--- src/main/java/org/apache/camel/splunk/demo/Tweet2SplunkEvent.java
+++ src/main/java/org/apache/camel/splunk/demo/Tweet2SplunkEvent.java
@@ -11,10 +11,20 @@ public class Tweet2SplunkEvent {
     public static SplunkEvent convertTweet(Status status) {
         SplunkEvent data = new SplunkEvent("twitter-message", "twitter");
         data.addPair("source", status.getSource());
-        data.addPair("from_user", status.getUser());
-        data.addPair("from_user_id", status.getId());
+        data.addPair("from_user", status.getUser().getScreenName());
+        data.addPair("in_reply_to", status.getInReplyToScreenName());
         data.addPair("created_at", status.getCreatedAt());
         data.addPair("text", status.getText());
+        data.addPair("retweet_count", status.getRetweetCount());
+        if (status.getPlace() != null) {
+            data.addPair("place_country", status.getPlace().getCountry());
+            data.addPair("place_name", status.getPlace().getName());
+            data.addPair("place_street", status.getPlace().getStreetAddress());
+        }
+        if (status.getGeoLocation() != null) {
+            data.addPair("geo_latitude", status.getGeoLocation().getLatitude());
+            data.addPair("geo_longitude", status.getGeoLocation().getLongitude());
+        }
         return data;
     }
 }
